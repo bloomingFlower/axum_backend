d71ff38213dc087c13810df8bc354f630e8ab382
AAII <sinsunby@gmail.com>
2024-05-12 23:27:37 +0900

    add comments

--- src/model.rs
+++ src/model.rs
@@ -40,30 +40,42 @@ impl ModelController {
 
 // CRUD Implementation
 impl ModelController {
+    /// Create Ticket
     pub async fn create_ticket(&self, ctx: Ctx, ticket_fc: TicketForCreate) -> Result<Ticket> {
+        // Tickets Store locked by Mutex
         let mut tickets_store = self.tickets_store.lock().unwrap();
+        // Set Ticket ID as the length of Tickets Store
         let id = tickets_store.len() as u64;
+        // Ticket Struct
         let ticket = Ticket {
             id,
             cid: ctx.user_id(),
             title: ticket_fc.title,
         };
+        // Push Ticket to Tickets Store
         tickets_store.push(Some(ticket.clone()));
-
+        // Return Ticket
         Ok(ticket)
     }
 
+    /// Get Tickets List
     pub async fn list_tickets(&self, _ctx: Ctx) -> Result<Vec<Ticket>> {
+        // Tickets Store locked by Mutex
         let tickets_store = self.tickets_store.lock().unwrap();
+        // Get Tickets from Tickets Store and filter out None values and collect them into a Vec
         let tickets = tickets_store.iter().filter_map(|t| t.clone()).collect();
-
+        // Return Tickets
         Ok(tickets)
     }
 
+    /// Delete Ticket by ID from Tickets Store and return the deleted Ticket
+    /// or an Error if the ID is not found in the Tickets Store
     pub async fn delete_ticket(&self, _ctx: Ctx, id: u64) -> Result<Ticket> {
+        // Tickets Store locked by Mutex
         let mut tickets_store = self.tickets_store.lock().unwrap();
+        // Get Ticket by ID from Tickets Store and take it out of the Option and return it or an Error if the ID is not found in the Tickets Store
         let ticket = tickets_store.get_mut(id as usize).and_then(|t| t.take());
-
+        // Return Ticket or Error
         ticket.ok_or(Error::TicketDeleteFailIdNotFound { id })
     }
 }
--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -13,45 +13,52 @@ use axum::RequestPartsExt;
 use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
+/// Middleware to require authentication for the request to continue to the next handler or middleware in the chain
 pub async fn mw_require_auth(ctx: Result<Ctx>, req: Request<Body>, next: Next) -> Result<Response> {
     println!("--> {:<12} - mw_require_auth - {ctx:?}", "MIDDLEWARE");
 
+    // Check if the Ctx was extracted correctly
     ctx?;
 
     // TODO: Token validation
 
+    // Continue to the next handler or middleware in the chain
     Ok(next.run(req).await)
 }
 
 // region: Ctx Extractor
+/// Implement the FromRequestParts trait for the Ctx type
 #[async_trait]
 impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    /// Error type for the Ctx Extractor
     type Rejection = Error;
 
+    /// Extract the Ctx from the request parts(Component parts of an HTTP Request)
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
         println!("--> {:<12} - Ctx", "EXTRACTOR");
-
+        // Extract the Cookies from the request parts
         let cookies = parts.extract::<Cookies>().await.unwrap();
-
+        // Extract the AUTH_TOKEN cookie from the cookies
         let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
-
+        // Parse the token into its parts
         let (user_id, _exp, _sign) = auth_token
             .ok_or(Error::AuthFailNoAuthTokenCookie)
             .and_then(parse_token)?; // FIXME: Implement a real token validation
-
+                                     // Create a new Ctx with the user_id and return it
         Ok(Ctx::new(user_id))
     }
 }
 // endregion: Ctx Extractor
 
-/// Parse a toekn from the cookies
+/// Parse a token from the cookies
 fn parse_token(token: String) -> Result<(u64, String, String)> {
+    // Parse the token into its parts
     let (_whole, _user_id, _exp, _sign) = regex_captures!(r#"^user-(\d+)\.(\w+)\.(\w+)"#, &token)
         .ok_or(Error::AuthFailTokenWrongFormat)?;
-
+    // Parse the user_id into a u64
     let user_id = _user_id
         .parse()
         .map_err(|_| Error::AuthFailTokenWrongFormat)?;
-
+    // Return the user_id, exp, and sign
     Ok((user_id, _exp.to_string(), _sign.to_string()))
 }
