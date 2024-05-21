e17c8c0d5932eaedd784a9c25906ed10d5c5f638
AAII <sinsunby@gmail.com>
2024-05-21 23:34:35 +0900

    unit test

--- src/_dev_utils/mod.rs
+++ src/_dev_utils/mod.rs
@@ -37,23 +37,23 @@ pub async fn init_test() -> ModelManager {
 
     mm.clone()
 }
-//
-// pub async fn seed_tasks(ctx: &Ctx, mm: &ModelManager, titles: &[&str]) -> model::Result<Vec<Task>> {
-//     let mut tasks = Vec::new();
-//
-//     for title in titles {
-//         let id = TaskBmc::create(
-//             ctx,
-//             mm,
-//             TaskForCreate {
-//                 title: title.to_string(),
-//             },
-//         )
-//         .await?;
-//         let task = TaskBmc::get(ctx, mm, id).await?;
-//
-//         tasks.push(task);
-//     }
-//
-//     Ok(tasks)
-// }
+
+pub async fn seed_tasks(ctx: &Ctx, mm: &ModelManager, titles: &[&str]) -> model::Result<Vec<Task>> {
+    let mut tasks = Vec::new();
+
+    for title in titles {
+        let id = TaskBmc::create(
+            ctx,
+            mm,
+            TaskForCreate {
+                title: title.to_string(),
+            },
+        )
+        .await?;
+        let task = TaskBmc::get(ctx, mm, id).await?;
+
+        tasks.push(task);
+    }
+
+    Ok(tasks)
+}
