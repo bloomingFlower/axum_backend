e3d829fcbc09723c2d1cd95bdc5103c99b2f4123
AAII <sinsunby@gmail.com>
2024-05-26 01:56:30 +0900

    password

--- sql/dev_initial/00-recreate-db.sql
+++ sql/dev_initial/00-recreate-db.sql
@@ -4,7 +4,6 @@ FROM pg_stat_activity
 WHERE pg_stat_activity.datname = 'dev_app';
 DROP DATABASE IF EXISTS dev_app;
 DROP USER IF EXISTS dev_app;
-
 -- DEV ONLY - Dev only password (for local dev and unit test)
 CREATE USER dev_app PASSWORD 'dev_app';
 CREATE DATABASE dev_app owner dev_app ENCODING = 'UTF8';
---
+++ src/model/user.rs
@@ -0,0 +1,105 @@
+use crate::ctx::Ctx;
+use crate::model::base::{self, DbBmc};
+use crate::model::{Error, ModelManager, Result};
+use serde::{Deserialize, Serialize};
+use sqlb::{Fields, HasFields};
+use sqlx::postgres::PgRow;
+use sqlx::FromRow;
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Fields, FromRow, Serialize)]
+pub struct User {
+    pub id: i64,
+    pub username: String,
+}
+
+#[derive(Deserialize)]
+pub struct UserForCreate {
+    pub username: String,
+    pub password_clear: String,
+}
+
+#[derive(Fields)]
+pub struct UserForInsert {
+    username: String,
+}
+
+#[derive(Clone, FromRow, Fields, Debug)]
+pub struct UserForLogin {
+    pub id: i64,
+    pub username: String,
+    pub password: Option<String>,
+    pub password_salt: Uuid,
+    pub token_salt: Uuid,
+}
+
+#[derive(Clone, FromRow, Fields, Debug)]
+pub struct UserForAuth {
+    pub id: i64,
+    pub username: String,
+    pub token_salt: Uuid,
+}
+
+pub trait UserBy: HasFields + for<'r> FromRow<'r, PgRow> + Unpin + Send {}
+
+impl UserBy for User {}
+impl UserBy for UserForLogin {}
+impl UserBy for UserForAuth {}
+
+pub struct UserBmc;
+
+impl DbBmc for UserBmc {
+    const TABLE: &'static str = "users";
+}
+
+impl UserBmc {
+    pub async fn get<E>(ctx: &Ctx, mm: &ModelManager, id: i64) -> Result<E>
+    where
+        E: UserBy,
+    {
+        base::get::<Self, _>(ctx, mm, id).await
+    }
+
+    pub async fn first_by_username<E>(
+        _ctx: &Ctx,
+        mm: &ModelManager,
+        username: &str,
+    ) -> Result<Option<E>>
+    where
+        E: UserBy,
+    {
+        let db = mm.db();
+
+        let user = sqlb::select()
+            .table(Self::TABLE)
+            .and_where("username", "=", username)
+            .fetch_optional::<_, E>(db)
+            .await?;
+
+        Ok(user)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::_dev_utils;
+    use anyhow::{Context, Result};
+    use serial_test::serial;
+
+    #[serial]
+    #[tokio::test]
+    async fn test_first_ok_demo1() -> Result<()> {
+        let mm = _dev_utils::init_test().await;
+        let ctx = Ctx::root_ctx();
+        let fx_username = "demo1";
+
+        let user: User = UserBmc::first_by_username(&ctx, &mm, fx_username)
+            .await?
+            .context("Should have user demo1")?;
+
+        assert_eq!(user.username, fx_username);
+
+        Ok(())
+    }
+}
